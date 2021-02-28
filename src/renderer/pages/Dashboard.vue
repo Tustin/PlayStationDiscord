e2571a2e02607ed4c78bcf6d64422a351e3d231e
Tustin <Tustin@users.noreply.github.com>
2021-02-28 17:56:04 -0600

    landing page

---
+++ src/renderer/pages/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="playstation-info">
+        <img
+            src="../assets/images/defaultAvatar.png"
+            id="avatar"
+            alt="Avatar"
+            draggable="false"
+        />
+        <div class="presence-details">
+            <h3 id="onlineId"></h3>
+            <p id="details">Offline</p>
+            <p id="state"></p>
+        </div>
+    </div>
+    <div class="options">
+        <button class="red" id="togglePresence">
+            Disable
+        </button>
+        <button class="blurple" id="signOut">
+            Sign Out
+        </button>
+    </div>
+</template>
